fb4edad1763c56ae436fa0393060b8b07bad23b0
Ananyo Roy Ghattak Chowdhury <rgc.ananyo@gmail.com>
2024-06-02 02:16:10 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import {Auth} from "./pages/auth/index";
+import { ExpenseTracker } from './pages/expense-tracker';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" exact element={<Auth/>}/>
+          <Route path="/expense-tracker" element={<ExpenseTracker />}/>
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
